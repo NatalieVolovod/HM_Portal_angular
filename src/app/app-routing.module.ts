89030ef01c3eee23866af71d9d46fe64c38e483b
NatalieVolovod <volovod.natalie@gmail.com>
2019-09-24 12:40:31 +0300

    added routing to artisans-list and masterclasses-list components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 import {ArticlesListComponent} from './articles/articles-list/articles-list.component';
 import {HomeComponent} from './home/home.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {ArtisansListComponent} from './artisans/artisans-list/artisans-list.component';
+import {MasterclassesListComponent} from './masterclasses/masterclasses-list/masterclasses-list.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'artisans', component: ArtisansListComponent },
+  { path: 'masterclasses', component: MasterclassesListComponent },
   { path: 'articles', component: ArticlesListComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent }
 
-  // { path: 'article/:id',      component: ArticleComponent }
-  // { path: 'articles', component: ArticleListComponent, data: { title: 'Articles List' } }  // -- ArticlesListComponent?
+  // TODO contacts page
+  // TODO { path: 'article/:id',      component: ArticleComponent }
+  // TODO { path: 'articles', component: ArticleListComponent, data: { title: 'Articles List' } }  // -- ArticlesListComponent?
 ];
 
 @NgModule({
