79ae5e682fe5f623462d8c3cd3f8b121768afd1b
NatalieVolovod <volovod.natalie@gmail.com>
2019-10-19 12:27:35 +0300

    article_changes

--- src/app/articles_old/article/article.component.ts
+++ src/app/articles_old/article/article.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  article = '<h1>Mandala weaving - an exciting lesson</h1><iframe width="560" height="315" src="https://www.youtube.com/embed/xObJ04gs0tY" frameborder="0" allowfullscreen></iframe>';
 
   constructor() { }
 
